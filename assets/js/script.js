38551ed718deccc74f6cee126a787da1705fe18a
Sunaljabili <jabilisunal@gmail.com>
2022-05-20 20:28:30 +0400

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// let btns = document.querySelectorAll(".btn");
+
+
+// let count = 0;
+// btns.forEach(btn => {
+//     btn.addEventListener("click", function () {
+//         let content = document.getElementById(btn.dataset.id);
+//         content.style.filter = "none";
+//         btn.style.visibility = "hidden"
+//         count++;
+//         if (count >=0) {
+//             btns.forEach(btn => btn.style.visibility = "hidden")
+//         }
+//     })
+// })
+
+
+// const area = document.querySelector(".area");
+// const circle = document.querySelector(".circle");
+
+// area.addEventListener("click", function (e) {
+//     circle.style.top = e.clientY - area.offsetTop / 2  - 100 + "px";
+//     circle.style.left = e.clientX - area.offsetLeft / 2 -100 + "px";
+// })
+
+
+
+
+// let navbar = document.querySelector(".navbar");
+
+// window.addEventListener("scroll",function(e){
+    
+
+// })
+
